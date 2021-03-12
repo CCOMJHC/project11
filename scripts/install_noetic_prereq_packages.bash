c31d3edc1ff83b620fe49001c77204630d71b11e
Marcos Barrera <marcos.barrera@seafloorsystems.com>
2021-03-12 15:55:14 -0800

    Noetic Prereq Patch
    Creation of Noetic prereq package installation bash script and associated documentation changes

---
+++ scripts/install_noetic_prereq_packages.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+apt install qtpositioning5-dev libqt5svg5-dev ros-noetic-geodesy ros-noetic-pid ros-noetic-joy ros-noetic-rosmon python3-gdal qtpositioning5-dev libqt5svg5-dev libgeos++-dev ros-noetic-image-transport ros-noetic-executive-smach
